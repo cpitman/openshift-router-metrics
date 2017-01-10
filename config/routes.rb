a59973ce5f0b33a3de974e0ddfaeb1312aeab05a
Chris Pitman <cpitman1@gmail.com>
2017-01-10 00:49:08 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'stats/index'
-
+  get 'auth/:provider/callback', to: 'sessions#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
