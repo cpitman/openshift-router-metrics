4d08d1ecd058bbad8e603f23ccbf0c15f36ed608
Chris Pitman <cpitman1@gmail.com>
2017-03-09 00:50:06 -0500

    Parameterize client_token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,14 +12,14 @@ class ApplicationController < ActionController::Base
   end
 
   def access_token
-    client = OAuth2::Client.new('router-stats', 'super-secret', {site: 'https://10.1.2.2:8443/', connection_opts: { ssl: {verify: false} } } )
+    client = OAuth2::Client.new(ENV['OAUTHCLIENT_NAME'], ENV['OAUTHCLIENT_SECRET'], {site: ENV['PUBLIC_MASTER_URL'], connection_opts: { ssl: {verify:  (ENV['VERIFY_MASTER_TLS'] || 'true') == 'true'} } } )
 
     OAuth2::AccessToken.new client, session[:credentials]['token']
   end
 
   def service_account
     service_account_token = IO.read('/var/run/secrets/kubernetes.io/serviceaccount/token')
-    Faraday.new 'https://10.1.2.2:8443', headers: {'Authorization': "Bearer #{service_account_token}"}, ssl: {verify: false}
+    Faraday.new ENV['PUBLIC_MASTER_URL'], headers: {'Authorization': "Bearer #{service_account_token}"}, ssl: {verify:  (ENV['VERIFY_MASTER_TLS'] || 'true') == 'true'}
   end
 
   helper_method :current_user
